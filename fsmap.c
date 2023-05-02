f515695a9f317da3712de1256077feaf8606967d
tmerc10 <thomasdmercurio@gmail.com>
2023-05-02 20:32:41 +0100

    first edit of fsmap

--- fsmap.c
+++ fsmap.c
@@ -25,11 +25,13 @@ main(int argc, char *argv[])
     fd = open(argv[1],O_RDONLY);
     if (fd == -1)
     {
-	fprintf(stderr,"Cannot open %s\n",argv[1]);
-	exit(-1);
+	    fprintf(stderr,"Cannot open %s\n",argv[1]);
+	    exit(-1);
     }
 
-    blockNo = 46;  // let's print the content of block 32
+    printf("map of %s\n", argv[1]);
+
+    blockNo = 1;  // Reading data from the first block
 
     // move file read position to BSIZE*blockNo from the start of the file
     lseek(fd,BSIZE*blockNo,SEEK_SET);
@@ -44,28 +46,25 @@ main(int argc, char *argv[])
     // let's print the block data as a hexadecimal "dump" ;)
     for (i = 0; i < 64; i++)
     {
-	//print file offset
-	printf ("%08x  ", i*16 + BSIZE*blockNo);
+    	// print 16 bytes of data in decimal
+    	for (j = 0; j < 16; j++)
+    	{
+    	    printf("%x ", block[i * 16 + j]);
 
-	// print 16 bytes of data in decimal
-	for (j = 0; j<16; j++)
-	{
-	    printf("%02x ",block[i*16+j]);
+    	    // print a separating ' ' after the first 8 bytes
+    	    if (j == 7) putchar(' ');
+    	}
 
-	    // print a separating ' ' after the first 8 bytes
-	    if (j == 7) putchar(' ');
-	}
+    	// print a few whitespaces separating hex from char section
+    	printf(" ");
 
-	// print a few whitespaces separating hex from char section
-	printf(" ");
-
-	//print16 bytes of data as characters replacing unprintable chars with '.'
-	for (j = 0; j<16; j++)
-	{
-	   c = ((block[i*16+j] >= ' ') && (block[i*16+j] <= '~')) ? block[i*16+j] : '.';
-	   putchar(c);
-	}
-	putchar('\n');
+    	//print16 bytes of data as characters replacing unprintable chars with '.'
+    	for (j = 0; j<16; j++)
+    	{
+    	   c = ((block[i*16+j] >= ' ') && (block[i*16+j] <= '~')) ? block[i*16+j] : '.';
+    	   putchar(c);
+    	}
+    	putchar('\n');
     }
     exit(0);
     //never gets here
