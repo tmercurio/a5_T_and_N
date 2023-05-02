1e45403f764e6e60f48a8d1ff154d8e3d17fe89a
tmerc10 <thomasdmercurio@gmail.com>
2023-05-02 11:03:42 +0100

    debug statements added

--- user/interprocess.c
+++ user/interprocess.c
@@ -108,7 +108,7 @@ void worker()
 	for (int j=0; j <100000; j++)
 	{
 	   s->a[i].value++;
-       if (a % 100 == 0)
+       if (j % 100 == 0)
         printf("Incrementing value (%d)\n", j);
 	}
 	// ---------------------------------------------------
