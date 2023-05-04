9d162eecc79647a64aea429ba5c9e13786e7f409
tmerc10 <thomasdmercurio@gmail.com>
2023-05-04 11:00:48 +0100

    interprocess updates

--- user/interprocess.c
+++ user/interprocess.c
@@ -118,7 +118,8 @@ void worker(int sem)
     sem_post((sem + 1) % M, 1);
    }
 
-   //sem_post((sem + 1) % M, 1);
+   if (sem != 0)
+    sem_post((sem + 1) % M, 1);
    printf("Waiting to exit process %d\n", sem);
 
    exit(0);
